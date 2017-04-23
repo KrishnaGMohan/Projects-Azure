1b76d80623bfb9622ef4888adb6d074a568c00b4
Krishna Gandhi Mohan <krgandhi@microsoft.com>
2017-04-23 17:20:53 -0400

    Commit 20170423 - 001

---
+++ DataUtilities/DataUtilities/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace DataUtilities
+{
+    public class Convertor
+    {
+        public static float BytesToKb(long? bytes)
+        {
+            return (float)bytes / 1000;
+        }
+    }
+}
